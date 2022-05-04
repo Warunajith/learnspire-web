1836f5334bbb0bb163e5d293f75c7390ce36630f
Malkishara <isurumalkishara97@gmail.com>
2022-05-04 17:43:44 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 import HomeComponent from './components/HomeComponent'
 import SignUpComponent from './components/SignUpComponent'
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
@@ -9,12 +10,17 @@ import LecturerViewComponent from './components/LecturerViewComponent';
 import AvailableCoursesComponenet from './components/AvailableCoursesComponenet';
 import EnrolledCoursesComponent from './components/EnrolledCoursesComponent';
 import CourseContentComponent from './components/CourseContentComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import AboutUs from './components/AboutUs';
+import Profile from './components/Profile';
+
 
 function App() {
   return (
     <Router>
         
-
+         <HeaderComponent/>
 
               <div className="container">
                 
@@ -23,17 +29,22 @@ function App() {
                   <Route path="/" exact element={<HomeComponent/>}></Route>
                   <Route path="/signup" element={<SignUpComponent/>}></Route>
                   <Route path="/signin" element={<LoginComponent/>}></Route>
-                  <Route path="/studentview" element={<StudentViewComponent/>}></Route>
-                  <Route path="/lecturerView" element={<LecturerViewComponent/>}></Route>
-                  <Route path="/availablecourses" element={<AvailableCoursesComponenet/>}></Route>
-                  <Route path="/enrolledcourses" element={<EnrolledCoursesComponent/>}></Route>
-                  <Route path="/coursecontent/:id" element={<CourseContentComponent/>}></Route>
+                  <Route path="/studentview" element={ <> <Navbar/> <StudentViewComponent /> <FooterComponent /></>}/>
+                  <Route path="/lecturerView" element={ <> <Navbar/> <LecturerViewComponent/> <FooterComponent /></>}/>
+                  <Route path="/available" element={ <> <Navbar/> <AvailableCoursesComponenet/> <FooterComponent /></>}/>
+                  <Route path="/enrolled" element={ <> <Navbar/> <EnrolledCoursesComponent/> <FooterComponent /></>}/>
+                  <Route path="/coursecontent/:id" element={ <> <Navbar/> <CourseContentComponent/> <FooterComponent /></>}/>
+                  <Route path="/aboutus" element={ <><Navbar/>  <AboutUs/> <FooterComponent /></>}/>
+                  <Route path="/profile" element={ <><Navbar/>  <Profile/> <FooterComponent /></>}/>
+                  
                  
                 </Routes>
                 
               </div>
+              
+              
 
-          
+         
 
         
       </Router>
---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Profile extends Component {
+  render() {
+    return (
+      <div>
+          <h1>My Profile</h1>
+        
+      </div>
+    )
+  }
+}
