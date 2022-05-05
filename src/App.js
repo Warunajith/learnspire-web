fabc67eb533b9bc5133158e7de0559ed9420a442
Warunajith <warunajithbandara@gmail.com>
2022-05-05 09:04:42 +0530

    Pulled

--- src/App.js
+++ src/App.js
@@ -10,19 +10,11 @@ import LecturerViewComponent from './components/LecturerViewComponent';
 import AvailableCoursesComponenet from './components/AvailableCoursesComponenet';
 import EnrolledCoursesComponent from './components/EnrolledCoursesComponent';
 import CourseContentComponent from './components/CourseContentComponent';
-<<<<<<< HEAD
 import AddNewCourseComponent from './components/AddNewCourseComponent';
 import AddAnnouncement from './components/AddAnnouncement';
 import EnrolledStudents from './components/EnrolledStudents';
 import AddContentComponent from './components/AddContentComponent';
 import ViewResultsComponent from './components/ViewResultsComponent';
-=======
-import HeaderComponent from './components/HeaderComponent';
-import FooterComponent from './components/FooterComponent';
-import AboutUs from './components/AboutUs';
-import Profile from './components/Profile';
-
->>>>>>> 2523557a3aa44e16996e623bfd853642d4715016
 
 function App() {
   return (
@@ -37,7 +29,6 @@ function App() {
                   <Route path="/" exact element={<HomeComponent/>}></Route>
                   <Route path="/signup" element={<SignUpComponent/>}></Route>
                   <Route path="/signin" element={<LoginComponent/>}></Route>
-<<<<<<< HEAD
                   <Route path="/studentview" element={<StudentViewComponent/>}></Route>
                   <Route path="/lecturerView" element={<LecturerViewComponent/>}></Route>
                   <Route path="/availablecourses" element={<AvailableCoursesComponenet/>}></Route>
@@ -49,17 +40,6 @@ function App() {
                   <Route path="/addcontent/:id" element={<AddContentComponent/>}></Route>
                   <Route path="/viewresults/" element={<ViewResultsComponent/>}></Route>
 
-=======
-                  <Route path="/studentview" element={ <> <Navbar/> <StudentViewComponent /> <FooterComponent /></>}/>
-                  <Route path="/lecturerView" element={ <> <Navbar/> <LecturerViewComponent/> <FooterComponent /></>}/>
-                  <Route path="/available" element={ <> <Navbar/> <AvailableCoursesComponenet/> <FooterComponent /></>}/>
-                  <Route path="/enrolled" element={ <> <Navbar/> <EnrolledCoursesComponent/> <FooterComponent /></>}/>
-                  <Route path="/coursecontent/:id" element={ <> <Navbar/> <CourseContentComponent/> <FooterComponent /></>}/>
-                  <Route path="/aboutus" element={ <><Navbar/>  <AboutUs/> <FooterComponent /></>}/>
-                  <Route path="/profile" element={ <><Navbar/>  <Profile/> <FooterComponent /></>}/>
-                  
-                 
->>>>>>> 2523557a3aa44e16996e623bfd853642d4715016
                 </Routes>
                 
               </div>
